343235ad0ea34e84875a2683f081a61606e60368
ipetrash <gil9red@gmail.com>
2015-11-11 13:48:07 +0500

    Генерация и рисование доски судоку

--- sudoku.py
+++ sudoku.py
@@ -3,126 +3,142 @@
 
 __author__ = 'ipetrash'
 
+import sys
 
 from PySide.QtGui import *
 from PySide.QtCore import *
 
-from random import randint
-import sys
+from utils import solver, sudoku_generator
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
 
-    class Widget(QWidget):
-        def __init__(self):
-            super().__init__()
-            self.setWindowTitle('Sudoku')
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Sudoku')
 
-            self.cell_size = 20
-            self.matrix_size = 9
+        self.cell_size = 20
+        self.matrix_size = 9
 
-            self.x_highlight_cell = -1
-            self.y_highlight_cell = -1
+        self.x_highlight_cell = -1
+        self.y_highlight_cell = -1
 
-            # Заполняем матрицу случайными числами от 0 до 9
-            self.matrix = list()
+        # TODO: числа, которые уже есть в матрице сделать неизменяемыми и с другим оформлением
+        self.matrix, self.sudoku_size = sudoku_generator.gen()
+
+        self.setMouseTracking(True)
+
+    def keyPressEvent(self, event):
+        super().keyPressEvent(event)
+
+        if event.key() == Qt.Key_Space:
+            # Получим список решения этой судоку
+            for solution in solver.solve_sudoku(self.sudoku_size, self.matrix):
+                # Берем самое первое
+                self.matrix = solution
+
+                # Перерисовываем окно
+                self.update()
+
+                break
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+
+        w, h = event.size().width(), event.size().height()
+        min_size = min(w, h)
+
+        self.cell_size = min_size // self.matrix_size
+
+    def mouseMoveEvent(self, event):
+        super().mouseMoveEvent(event)
+
+        pos = event.pos()
+
+        self.x_highlight_cell = pos.x() // self.cell_size
+        self.y_highlight_cell = pos.y() // self.cell_size
+
+        self.update()
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+
+        painter = QPainter(self)
+
+        # Если индекс ячейки под курсором валидный
+        if 0 <= self.x_highlight_cell < self.matrix_size and 0 <= self.y_highlight_cell < self.matrix_size:
+            # Выделение всего столбца и строки пересекающих ячейку под курсором
+            painter.save()
+            painter.setBrush(Qt.lightGray)
+
+            # Выделение строки
             for i in range(self.matrix_size):
-                row = list()
-                self.matrix.append(row)
-
-                for j in range(self.matrix_size):
-                    row.append(randint(0, 9))
-
-            self.setMouseTracking(True)
-
-        def resizeEvent(self, event):
-            super().resizeEvent(event)
-
-            w, h = event.size().width(), event.size().height()
-            min_size = min(w, h)
-
-            self.cell_size = min_size // self.matrix_size
-
-        def mouseMoveEvent(self, event):
-            super().mouseMoveEvent(event)
-
-            pos = event.pos()
-
-            self.x_highlight_cell = pos.x() // self.cell_size
-            self.y_highlight_cell = pos.y() // self.cell_size
-
-            self.update()
-
-        def paintEvent(self, event):
-            super().paintEvent(event)
-
-            painter = QPainter(self)
-
-            # Если индекс ячейки под курсором валидный
-            if 0 <= self.x_highlight_cell < self.matrix_size and 0 <= self.y_highlight_cell < self.matrix_size:
-                # Выделение всего столбца и строки пересекающих ячейку под курсором
-                painter.save()
-                painter.setBrush(Qt.lightGray)
-
-                # Выделение строки
-                for i in range(self.matrix_size):
-                    painter.drawRect(i * self.cell_size,
-                                     self.y_highlight_cell * self.cell_size,
-                                     self.cell_size,
-                                     self.cell_size)
-
-                # Выделение столбца
-                for j in range(self.matrix_size):
-                    painter.drawRect(self.x_highlight_cell * self.cell_size,
-                                     j * self.cell_size,
-                                     self.cell_size,
-                                     self.cell_size)
-
-                painter.restore()
-
-                # Выделение ячейки под курсором
-                painter.save()
-                painter.setBrush(Qt.yellow)
-                painter.drawRect(self.x_highlight_cell * self.cell_size,
+                painter.drawRect(i * self.cell_size,
                                  self.y_highlight_cell * self.cell_size,
                                  self.cell_size,
                                  self.cell_size)
-                painter.restore()
 
-            # Рисование цифр в ячейки таблицы
-            for i in range(self.matrix_size):
-                for j in range(self.matrix_size):
-                    num = str(self.matrix[i][j])
+            # Выделение столбца
+            for j in range(self.matrix_size):
+                painter.drawRect(self.x_highlight_cell * self.cell_size,
+                                 j * self.cell_size,
+                                 self.cell_size,
+                                 self.cell_size)
 
-                    # Алгоритм изменения размера текста взят из http://stackoverflow.com/a/2204501
-                    # Для текущего пришлось немного адаптировать
-                    factor = (self.cell_size / 2) / painter.fontMetrics().width(num)
-                    if factor < 1 or factor > 1.25:
-                        f = painter.font()
-                        point_size = f.pointSizeF() * factor
-                        if point_size > 0:
-                            f.setPointSizeF(point_size)
-                            painter.setFont(f)
+            painter.restore()
 
-                    x = i * self.cell_size
-                    y = j * self.cell_size
-                    w, h = self.cell_size, self.cell_size
-                    painter.drawText(x, y, w, h, Qt.AlignCenter, num)
+            # Выделение ячейки под курсором
+            painter.save()
+            painter.setBrush(Qt.yellow)
+            painter.drawRect(self.x_highlight_cell * self.cell_size,
+                             self.y_highlight_cell * self.cell_size,
+                             self.cell_size,
+                             self.cell_size)
+            painter.restore()
 
-            # Рисование сетки таблицы
-            y1, y2 = 0, 0
+        # Рисование цифр в ячейки таблицы
+        for i in range(self.matrix_size):
+            for j in range(self.matrix_size):
+                num = self.matrix[i][j]
 
-            for i in range(self.matrix_size + 1):
-                painter.drawLine(0, y1, self.cell_size * self.matrix_size, y2)
-                y1 += self.cell_size
-                y2 += self.cell_size
+                # Проверяем, что число не находится в диапазоне от 1 до 9
+                if not 1 <= num <= 9:
+                    continue
 
-            x1, x2 = 0, 0
+                num = str(num)
 
-            for i in range(self.matrix_size + 1):
-                painter.drawLine(x1, 0, x2, self.cell_size * self.matrix_size)
-                x1 += self.cell_size
-                x2 += self.cell_size
+                # Алгоритм изменения размера текста взят из http://stackoverflow.com/a/2204501
+                # Для текущего пришлось немного адаптировать
+                factor = (self.cell_size / 2) / painter.fontMetrics().width(num)
+                if factor < 1 or factor > 1.25:
+                    f = painter.font()
+                    point_size = f.pointSizeF() * factor
+                    if point_size > 0:
+                        f.setPointSizeF(point_size)
+                        painter.setFont(f)
+
+                x = i * self.cell_size
+                y = j * self.cell_size
+                w, h = self.cell_size, self.cell_size
+                painter.drawText(x, y, w, h, Qt.AlignCenter, num)
+
+        # Рисование сетки таблицы
+        y1, y2 = 0, 0
+
+        for i in range(self.matrix_size + 1):
+            painter.drawLine(0, y1, self.cell_size * self.matrix_size, y2)
+            y1 += self.cell_size
+            y2 += self.cell_size
+
+        x1, x2 = 0, 0
+
+        for i in range(self.matrix_size + 1):
+            painter.drawLine(x1, 0, x2, self.cell_size * self.matrix_size)
+            x1 += self.cell_size
+            x2 += self.cell_size
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
 
     w = Widget()
     w.resize(200, 200)
---
+++ utils/sudoku_generator.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'denisbalyko'
+
+# Взято и портировано: http://habrahabr.ru/post/192102/
+
+import random
+
+from utils import solver
+
+DEBUG = False
+
+
+class grid:
+    def __init__(self, n=3):
+        """ Generation of the base table """
+        self.n = n
+        self.table = [
+            [
+                ((i * n + i // n + j) % (n * n) + 1)
+                for j in range(n * n)
+            ]
+            for i in range(n * n)
+        ]
+
+        if DEBUG:
+            print("The base table is ready!")
+
+    def show(self):
+        for row in self.table:
+            print(row)
+
+    def transposing(self):
+        """ Transposing the whole grid """
+
+        self.table = map(list, zip(*self.table))
+        self.table = list(self.table)
+
+    def swap_rows_small(self):
+        """ Swap the two rows """
+        area = random.randrange(0, self.n, 1)
+        line1 = random.randrange(0, self.n, 1)
+        # получение случайного района и случайной строки
+        N1 = area * self.n + line1
+        # номер 1 строки для обмена
+
+        line2 = random.randrange(0, self.n, 1)
+        # случайная строка, но не та же самая
+        while line1 == line2:
+            line2 = random.randrange(0, self.n, 1)
+
+        N2 = area * self.n + line2
+
+        # номер 2 строки для обмена
+        self.table[N1], self.table[N2] = self.table[N2], self.table[N1]
+
+    def swap_colums_small(self):
+        self.transposing()
+        self.swap_rows_small()
+        self.transposing()
+
+    def swap_rows_area(self):
+        """ Swap the two area horizon """
+        area1 = random.randrange(0, self.n, 1)
+        # получение случайного района
+
+        area2 = random.randrange(0, self.n, 1)
+        # ещё район, но не такой же самый
+        while area1 == area2:
+            area2 = random.randrange(0, self.n, 1)
+
+        for i in range(0, self.n):
+            N1, N2 = area1 * self.n + i, area2 * self.n + i
+            self.table[N1], self.table[N2] = self.table[N2], self.table[N1]
+
+    def swap_colums_area(self):
+        self.transposing()
+        self.swap_rows_area()
+        self.transposing()
+
+    def mix(self, amt=10):
+        mix_func = ['self.transposing()',
+                    'self.swap_rows_small()',
+                    'self.swap_colums_small()',
+                    'self.swap_rows_area()',
+                    'self.swap_colums_area()']
+        for i in range(1, amt):
+            id_func = random.randrange(0, len(mix_func), 1)
+            eval(mix_func[id_func])
+
+
+def gen():
+    """Функция генерации таблицы судоку."""
+
+    example = grid()
+    example.mix()
+
+    flook = [
+        [0 for j in range(example.n * example.n)]
+        for i in range(example.n * example.n)
+    ]
+    iterator = 0
+    difficult = example.n ** 4  # Первоначально все элементы на месте
+
+    if DEBUG:
+        example.show()
+        print("---------------------------")
+
+    while iterator < example.n ** 4:
+        # Выбираем случайную ячейку
+        i, j = random.randrange(0, example.n * example.n, 1), random.randrange(0, example.n * example.n, 1)
+        if flook[i][j] == 0:  # Если её не смотрели
+            iterator += 1
+            flook[i][j] = 1  # Посмотрим
+
+            temp = example.table[i][j]  # Сохраним элемент на случай если без него нет решения или их слишком много
+            example.table[i][j] = 0
+            difficult -= 1  # Усложняем если убрали элемент
+
+            table_solution = []
+            for copy_i in range(0, example.n * example.n):
+                table_solution.append(example.table[copy_i][:])  # Скопируем в отдельный список
+
+            i_solution = 0
+            for solution in solver.solve_sudoku((example.n, example.n), table_solution):
+                i_solution += 1  # Считаем количество решений
+
+            if i_solution != 1:  # Если решение не одинственное вернуть всё обратно
+                example.table[i][j] = temp
+                difficult += 1  # Облегчаем
+
+    if DEBUG:
+        example.show()
+        print("difficult = ", difficult)
+
+    return example.table, (example.n, example.n)
